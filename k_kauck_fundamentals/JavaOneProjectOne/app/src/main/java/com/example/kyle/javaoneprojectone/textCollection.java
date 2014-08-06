01354ef624a4eafbfa8ee0a525a7bd5d89c9e324
Kyle K. <kyle0nlin3@fullsail.edu>
2014-08-06 17:10:32 -0600

    Completed now 8/12 Objectives
    August 6, 2014

--- k_kauck_fundamentals/JavaOneProjectOne/app/src/main/java/com/example/kyle/javaoneprojectone/textCollection.java
+++ k_kauck_fundamentals/JavaOneProjectOne/app/src/main/java/com/example/kyle/javaoneprojectone/textCollection.java
@@ -1,13 +1,16 @@
 package com.example.kyle.javaoneprojectone;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.HashSet;
 
@@ -16,7 +19,9 @@ public class textCollection extends Activity {
 
     final String TAG = "Java One Project One";
     private TextView mUserInput;
+    private TextView mDataCount;
     private HashSet<String> collectedText = new HashSet<String>();
+    private int currentCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,19 @@ public class textCollection extends Activity {
         setContentView(R.layout.activity_text_collection);
 
         mUserInput = (TextView) findViewById(R.id.enteredText);
+        mDataCount = (TextView) findViewById(R.id.dataEntries);
+
+        //Creates my Save Alert Toast
+        final Context saveContext = getApplicationContext();
+        final CharSequence display = "Successfully Saved Entry";
+        final int duration = Toast.LENGTH_SHORT;
+        final Toast saveAlert = Toast.makeText(saveContext, display, duration);
+
+        //Creates my Duplicate Alert Toast
+        Context duplicateContext = getApplicationContext();
+        CharSequence duplicateDisplay = "Duplicate Item Found, Entry Not Saved";
+        int duplicateDuration = Toast.LENGTH_SHORT;
+        final Toast duplicateAlert = Toast.makeText(duplicateContext,duplicateDisplay, duplicateDuration);
 
         Button submitButton = (Button) findViewById(R.id.submitButton);
 
@@ -31,9 +49,28 @@ public class textCollection extends Activity {
             @Override
             public void onClick(View v) {
 
+                //This will hide the keyboard from the user when they click the submit button
+                InputMethodManager keyboardHide = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                keyboardHide.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
                 Log.i(TAG, "Button Clicked");
                 String currentText = mUserInput.getText().toString();
                 collectedText.add(currentText);
+
+                int standardCount = currentCount;
+                currentCount = collectedText.size();
+                String currentCountString = Integer.toString(currentCount);
+                mDataCount.setText("Current Number of Entries Is: " + currentCountString);
+
+                if (standardCount == currentCount){
+
+                    duplicateAlert.show();
+
+                } else {
+
+                    saveAlert.show();
+
+                }
 
                 mUserInput.setText("");
 
