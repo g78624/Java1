b8bc1f8c76cf4b100f192d5850473f2e0341e5ab
Kyle K. <kyle0nlin3@fullsail.edu>
2014-08-17 10:31:42 -0600

    Second Week 3 Commit
    Made Portrait View a Spinner, and Landscape a List View. Added in
    Detail View Display and Class. Both working as intended when tried with
    a button.

--- advancedviews/AdvancedViews/app/src/main/java/com/example/kyle/advancedviews/advancedViewMaster.java
+++ advancedviews/AdvancedViews/app/src/main/java/com/example/kyle/advancedviews/advancedViewMaster.java
@@ -3,7 +3,6 @@ package com.example.kyle.advancedviews;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class advancedViewMaster extends Activity {
@@ -12,7 +11,6 @@ public class advancedViewMaster extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advanced_view_master);
-        //setContentView(R.layout.main);
     }
 
 
@@ -23,15 +21,4 @@ public class advancedViewMaster extends Activity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ advancedviews/AdvancedViews/app/src/main/java/com/example/kyle/advancedviews/detailView.java
@@ -0,0 +1,18 @@
+package com.example.kyle.advancedviews;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kyle on 8/17/2014.
+ */
+public class detailView extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_view);
+
+    }
+
+}
