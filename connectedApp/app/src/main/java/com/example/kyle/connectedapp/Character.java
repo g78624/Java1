4a09e661b1eed0ca83b540475423b39193065c24
Kyle K. <kyle0nlin3@fullsail.edu>
2014-08-24 18:15:36 -0600

    Completed 7/8 Tasks
    August 24/2014 - Completed 7/8 Tasks, only need to add advanced
    view/library to enhance UI

---
+++ connectedApp/app/src/main/java/com/example/kyle/connectedapp/Character.java
@@ -0,0 +1,121 @@
+package com.example.kyle.connectedapp;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+public class Character {
+
+    final String TAG = "Character Class";
+
+    private String mName;
+    private Integer mLevel;
+    private String mServer;
+    private String mBattlegroup;
+    private Integer mAchievements;
+    private Integer mHonorableKills;
+
+    public Character(){}
+
+    public Character(String name, Integer level, String server, String battlegroup, Integer achievements, Integer honorableKills){
+
+        mName = name;
+        mLevel = level;
+        mServer = server;
+        mBattlegroup = battlegroup;
+        mAchievements = achievements;
+        mHonorableKills = honorableKills;
+
+    }
+
+    public Character(JSONObject characterData){
+
+        try{
+
+            mName = characterData.getString("name");
+            mLevel = characterData.getInt("level");
+            mServer = characterData.getString("realm");
+            mBattlegroup = characterData.getString("battlegroup");
+            mAchievements = characterData.getInt("achievementPoints");
+            mHonorableKills = characterData.getInt("totalHonorableKills");
+
+        } catch (Exception e){
+
+            Log.e(TAG, "This really didn't work so well!");
+
+        }
+
+    }
+
+    public String getName(){
+
+        return mName;
+
+    }
+
+    public void setName(String mName){
+
+        this.mName = mName;
+
+    }
+
+    public Integer getLevel(){
+
+        return mLevel;
+
+    }
+
+    public void setLevel(Integer mLevel){
+
+        this.mLevel = mLevel;
+
+    }
+
+    public String getServer(){
+
+        return mServer;
+
+    }
+
+    public void setServer(String mServer){
+
+        this.mServer = mServer;
+
+    }
+
+    public String getBattlegroup(){
+
+        return mBattlegroup;
+
+    }
+
+    public void setBattlegroup(String mBattlegroup){
+
+        this.mBattlegroup = mBattlegroup;
+
+    }
+
+    public Integer getAchievements(){
+
+        return mAchievements;
+
+    }
+
+    public void setAchievements(Integer mAchievements){
+
+        this.mAchievements = mAchievements;
+
+    }
+
+    public Integer getHonorableKills(){
+
+        return mHonorableKills;
+
+    }
+
+    public void setHonorableKills(Integer mHonorableKills){
+
+        this.mHonorableKills = mHonorableKills;
+
+    }
+}
--- connectedApp/app/src/main/java/com/example/kyle/connectedapp/DataActivity.java
+++ connectedApp/app/src/main/java/com/example/kyle/connectedapp/DataActivity.java
@@ -1,19 +1,152 @@
 package com.example.kyle.connectedapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class DataActivity extends Activity {
+
+    final String TAG = "Warcraft Character App";
+    private TextView mServerView;
+    private TextView mCharacterView;
+    ProgressBar mDataProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+
+        mServerView = (TextView) findViewById(R.id.serverInput);
+        mCharacterView = (TextView) findViewById(R.id.toonInput);
+        mDataProgress = (ProgressBar) findViewById(R.id.dataProgress);
+        mDataProgress.setVisibility(View.INVISIBLE);
+
+        Button searchButton = (Button) findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (networkConnection()) {
+
+                    InputMethodManager hideKeyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    hideKeyboard.hideSoftInputFromWindow(mCharacterView.getWindowToken(), 0);
+
+                    String serverName = mServerView.getText().toString().replace(" ", "-");
+                    String characterName = mCharacterView.getText().toString();
+
+                    try {
+
+                        String starterURL = "http://us.battle.net/api/wow/character/";
+                        URL getInfoURL = new URL(starterURL + serverName + "/" + characterName);
+                        new GetCharacterInfo().execute(getInfoURL);
+
+                    } catch (Exception e) {
+
+                        Log.e(TAG, "Invalid Information: " + serverName + " or " + characterName);
+
+                    }
+
+                } else {
+
+                    InputMethodManager hideKeyboard = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    hideKeyboard.hideSoftInputFromWindow(mCharacterView.getWindowToken(), 0);
+
+                    Toast.makeText(getApplicationContext(), "No Network Available", Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
     }
 
+    protected boolean networkConnection() {
+
+        ConnectivityManager myConnection = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo myNetwork = myConnection.getActiveNetworkInfo();
+
+        return myNetwork != null && myNetwork.isConnectedOrConnecting();
+    }
+
+    private class GetCharacterInfo extends AsyncTask<URL, Integer, JSONObject> {
+
+        final String TAG = "World of Warcraft Data Fetch";
+
+        @Override
+        protected void onPreExecute(){
+
+            mDataProgress.setVisibility(View.VISIBLE);
+
+        }
+
+        @Override
+        protected JSONObject doInBackground(URL... urls) {
+
+            String jsonInfo = "";
+
+            for (URL getInfoURL : urls) {
+
+                //publishProgress();
+
+                try {
+
+                    URLConnection connect = getInfoURL.openConnection();
+                    jsonInfo = IOUtils.toString(connect.getInputStream());
+                    break;
+
+                } catch (Exception e) {
+
+                    Log.e(TAG, "Could not establish a connection to " + getInfoURL.toString());
+
+                }
+
+            }
+
+            Log.i(TAG, "Received Data: " + jsonInfo);
+
+            JSONObject characterData;
+
+            try {
+
+                characterData = new JSONObject(jsonInfo);
+
+            } catch (Exception e) {
+
+                Log.e(TAG, "Cannot convert response");
+                characterData = null;
+
+            }
+
+            return characterData;
+
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject characterData) {
+
+            Character newCharacter = new Character(characterData);
+            mDataProgress.setVisibility(View.INVISIBLE);
+
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,15 +155,4 @@ public class DataActivity extends Activity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
